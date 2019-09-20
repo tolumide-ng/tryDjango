9250bd674fbe11a8fd3fddf0d6389e384e4fe007
tolumide-ng <tolumideshopein@gmail.com>
2019-09-20 10:53:31 +0100

    first---commit

---
+++ products/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+class Product(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    price = models.TextField()
